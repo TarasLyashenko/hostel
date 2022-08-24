e3a4a970cb59942203d06d2495746343a7db247d
Slayfi <d.d.lyashenko@gmail.com>
2022-08-24 20:20:51 +0400

    master: init commit

---
+++ src/main/java/com/liashenko/animalhotel/AnimalhotelApplication.java
@@ -0,0 +1,13 @@
+package com.liashenko.animalhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalhotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalhotelApplication.class, args);
+	}
+
+}
